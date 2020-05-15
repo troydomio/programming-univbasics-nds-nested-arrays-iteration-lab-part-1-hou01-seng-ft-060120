6db1bb55d27bce8978328e0e6c7088dc0536369f
Troy Domio <troyakins99@yahoo.com>
2020-05-15 03:15:14 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ def find_even_values(src)
     inner_count = 0
     while inner_count < src[count].length do
       if src[count][inner_count].even?
-     p src[count][inner_count]
+       p src[count][inner_count]
       inner_count += 1 
     end
     count += 10
